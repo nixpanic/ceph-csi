3fd08e0826bd22050fa5b0bd9cf309fcfb6e3ada
Niels de Vos <ndevos@redhat.com>
2020-01-03 15:44:12 +0100

    tests: allow running with a different user

--- scripts/travis-functest.sh
+++ scripts/travis-functest.sh
@@ -9,7 +9,7 @@ sudo scripts/minikube.sh deploy-rook
 # pull docker images to speed up e2e
 sudo scripts/minikube.sh cephcsi
 sudo scripts/minikube.sh k8s-sidecar
-sudo chown -R travis: "$HOME"/.minikube /usr/local/bin/kubectl
+sudo chown -R ${USER}: "$HOME"/.minikube /usr/local/bin/kubectl
 # functional tests
 
 go test github.com/ceph/ceph-csi/e2e --deploy-timeout=10 -timeout=30m -v
