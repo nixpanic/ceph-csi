76da1845dc78f81055968bccd528abb12036aab6
Humble Chirammal <hchiramm@redhat.com>
2020-06-19 12:16:16 +0530

    build: add CEPH_TAG option for scripts/test-go.sh
    Signed-off-by: Humble Chirammal <hchiramm@redhat.com>

--- scripts/test-go.sh
+++ scripts/test-go.sh
@@ -3,10 +3,32 @@
 GOPACKAGES="$(go list -mod=vendor ./... | grep -v -e vendor -e e2e)"
 COVERFILE="${GO_COVER_DIR}/profile.cov"
 
+
+case "${1:-}" in
+mimic)
+    CEPH_TAG="mimic"
+    ;;
+nautilus)
+    CEPH_TAG="nautilus"
+    ;;
+octopus)
+    CEPH_TAG="octopus"
+    ;;
+*)
+    echo " $0 [command]
+Available Commands:
+  mimic               set ceph tag to mimic for go test
+  nautilus            set ceph tag to nautilus for go test
+  octopus             set ceph tag to octopus for go test
+" >&2
+    ;;
+esac
+
+
 # no special options, exec to go test w/ all pkgs
 if [[ ${TEST_EXITFIRST} != "yes" && -z ${TEST_COVERAGE} ]]; then
 	# shellcheck disable=SC2086
-	exec go test -tags nautilus -mod=vendor -v ${GOPACKAGES}
+	exec go test -tags ${CEPH_TAG} -mod=vendor -v ${GOPACKAGES}
 fi
 
 # our options are set so we need to handle each go package one
@@ -20,7 +42,7 @@ failed=0
 for gopackage in ${GOPACKAGES}; do
 	echo "--- testing: ${gopackage} ---"
 	# shellcheck disable=SC2086
-	go test -tags nautilus -mod=vendor -v ${GOTESTOPTS} "${gopackage}" || ((failed += 1))
+	go test ${CEPH_TAG} -mod=vendor -v ${GOTESTOPTS} "${gopackage}" || ((failed += 1))
 	if [[ -f cover.out ]]; then
 		# Append to coverfile
 		grep -v "^mode: count" cover.out >>"${COVERFILE}"
