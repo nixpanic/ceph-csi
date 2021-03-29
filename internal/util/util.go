2ee6884ae7f9b454e3a06f8e690e3b632eb0f536
Niels de Vos <ndevos@redhat.com>
2021-03-29 11:38:47 +0200

    util: log available configs when KMS not found
    When the KMS configuration can not be found, it is useful to know what
    configurations are available. This aids troubleshooting when typos in
    the KMS ID are made.
    
    Signed-off-by: Niels de Vos <ndevos@redhat.com>

--- internal/util/util.go
+++ internal/util/util.go
@@ -319,3 +319,15 @@ func contains(s []string, key string) bool {
 
 	return false
 }
+
+func getKeys(m map[string]interface{}) []string {
+	keys := make([]string, len(m))
+
+	i := 0
+	for k := range m {
+		keys[i] = k
+		i++
+	}
+
+	return keys
+}
